878da31c1f60ad5724677eeab9b9b1b9566840b1
Noza-e <kyraluna.neo@gmail.com>
2025-03-03 17:28:12 +0100

    begin model

--- include/model.hpp
+++ include/model.hpp
@@ -9,24 +9,24 @@
 using namespace std;
 typedef unsigned short int usint;
 
-//
-class Player{
+
+class Player {
 
     private:
 
-        const string name;
-        const usint color;
+        const string name; // name of the player
+        const usint color; // color of the player
 
     public:
 
         // Constructor
-        Player(const string player_name, const int player_color)
+        Player(const string player_name, const usint player_color)
             : name(player_name), color(player_color) {};  // Automatic variable initialisation
 };
 
 
-//
-class GameModel{
+
+class GameModel {
 
     private:
 
@@ -42,3 +42,48 @@ class GameModel{
         GameModel() {};
 
 };
+
+
+class Unit {
+    
+    private:
+
+        usint color;  // owner of the units
+        usint defense; // level of the unit
+
+    public:
+
+        // Constructor
+        Unit(usint unit_color, usint unit_defense)
+            : color(unit_color), defense(unit_defense) {};  // Automatic variable initialisation
+        
+        // Getters
+        usint _color() const;
+        usint _defense() const;
+
+        // Functions
+        void upgrade(); // Increase unit defense
+        void convert_bandit(); // Change the unit into a bandits
+};
+
+class Building {
+
+    private:
+
+        usint color;  // owner of the building    
+        usint defense; // level of the building
+
+    public:
+
+        // Constructor
+        Building(usint building_color, usint building_defense)
+            : color(building_color), defense(building_defense) {};  // Automatic variable initialisation
+        
+        // Getters
+        usint _color() const;
+        usint _defense() const;
+
+        // Functions
+        
+};
+
--- src/model.cpp
+++ src/model.cpp
@@ -1,4 +1,14 @@
-//
 #include "model.hpp"
 
+// Class Unit
 
+usint Unit::_color() const { return color; }
+usint Unit::_defense() const { return defense; }
+
+void Unit::upgrade() { defense++; }
+void Unit::convert_bandit() { color = 0; defense = 1; }
+
+// Class Building
+
+usint Building::_color() const { return color; }
+usint Building::_defense() const { return defense; }
