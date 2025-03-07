597b8ec5d33643f637de28a681485533b0571d44
Noza-e <kyraluna.neo@gmail.com>
2025-03-07 11:59:08 +0100

    add content in model.hpp and model.cpp

--- include/model.hpp
+++ include/model.hpp
@@ -3,11 +3,26 @@
 //
 #include <iostream>
 #include <map>
+#include <vector>
 #include <string>
 #include <string_view>
 
 using namespace std;
 typedef unsigned short int usint;
+
+// type of tiles
+typedef enum Ground {
+    LOCK, // can't be possessed
+    NEUTRAL, // can be possessed
+    // possessed by player 1-4:
+    P1, 
+    P2,
+    P3,
+    P4
+} Ground;
+
+
+// ========== [ Players ] ==========
 
 
 class Player {
@@ -25,6 +40,111 @@ class Player {
 };
 
 
+// ========== [ Game Elements ] ==========
+
+
+class Element {
+    
+    protected:
+
+        usint color;  // owner of the element 
+        usint defense; // level (= defense) of the element
+
+    public:
+
+        // Constructor
+        Element () {}; // Default constructor
+        // Destructor
+        virtual ~Element() {}; // Virtual destructor
+
+        // Getters
+        virtual usint _color() const;
+        virtual usint _defense() const;
+};
+
+
+class Unit : public Element {
+
+    private:
+
+        usint color;
+        usint defense;
+
+    public:
+
+        // Constructor
+        Unit(usint unit_color, usint unit_defense)
+            : color(unit_color), defense(unit_defense) {};
+        // Destructor
+        ~Unit() {}; // Default destructor
+
+        // Functions
+        void upgrade(); // Increase unit defense
+        void convert_bandit(); // Change the unit into a bandits
+};
+
+
+class Building : public Element {
+
+    private:
+
+        usint color;
+        usint defense;
+
+    public:
+
+        // Constructor
+        Building(usint building_color, usint building_defense)
+            : color(building_color), defense(building_defense) {};
+        // Destructor
+        ~Building() {}; // Default destructor
+
+        // Functions
+        
+};
+
+
+// ========== [ Map ] ==========
+
+
+class Tile {
+
+    private:
+
+        usint x;
+        usint y;
+        Ground type;
+
+    public:
+
+        // Constructor
+        Tile(usint tile_x, usint tile_y, Ground tile_type=NEUTRAL)
+            : x(tile_x), y(tile_y), type(tile_type) {};
+        // Destructor
+        ~Tile() {}; // Default destructor
+
+        // Getters
+        usint _x() const;
+        usint _y() const;
+        Ground _type() const;
+
+        // Functions
+        void convert_type(Ground new_type);
+        bool is_adjacent(const Tile* tile);
+};
+
+
+class Map {
+
+    private:
+
+        vector<Tile*> tiles_layer;
+        vector<Element*> elements_layer;
+
+    public:
+};
+
+
 
 class GameModel {
 
@@ -32,6 +152,7 @@ class GameModel {
 
         // Players are linked here, indexed by there name
         map<string, Player*> players;
+        Map game_map;
 
     public:
 
@@ -40,50 +161,7 @@ class GameModel {
 
         // Constructor
         GameModel() {};
-
 };
 
 
-class Unit {
-    
-    private:
-
-        usint color;  // owner of the units
-        usint defense; // level of the unit
-
-    public:
-
-        // Constructor
-        Unit(usint unit_color, usint unit_defense)
-            : color(unit_color), defense(unit_defense) {};  // Automatic variable initialisation
-        
-        // Getters
-        usint _color() const;
-        usint _defense() const;
-
-        // Functions
-        void upgrade(); // Increase unit defense
-        void convert_bandit(); // Change the unit into a bandits
-};
-
-class Building {
-
-    private:
-
-        usint color;  // owner of the building    
-        usint defense; // level of the building
-
-    public:
-
-        // Constructor
-        Building(usint building_color, usint building_defense)
-            : color(building_color), defense(building_defense) {};  // Automatic variable initialisation
-        
-        // Getters
-        usint _color() const;
-        usint _defense() const;
-
-        // Functions
-        
-};
 
--- src/model.cpp
+++ src/model.cpp
@@ -1,14 +1,33 @@
 #include "model.hpp"
 
-// Class Unit
+// Elements
 
-usint Unit::_color() const { return color; }
-usint Unit::_defense() const { return defense; }
+usint Element::_color() const { return color; }
+usint Element::_defense() const { return defense; }
 
 void Unit::upgrade() { defense++; }
 void Unit::convert_bandit() { color = 0; defense = 1; }
 
-// Class Building
+// Map
 
-usint Building::_color() const { return color; }
-usint Building::_defense() const { return defense; }
+usint Tile::_x() const { return x; }
+usint Tile::_y() const { return y; }
+Ground Tile::_type() const { return type; }
+
+void Tile::convert_type(Ground new_type) { type = new_type; }
+
+bool Tile::is_adjacent(const Tile* tile) {
+    usint diff_x = abs(x - tile->_x());
+    usint diff_y = abs(y - tile->_y());
+    if ((diff_x <= 1) && (diff_y <= 1) && (diff_x + diff_y > 0)) {
+        if (diff_x == 1 && diff_y == 1) {
+            if ((tile->_x() < x) && (tile->_y() > y) || (tile->_x() > x) && (tile->_y() < y)) {
+                return false;
+            }
+        return true;
+        }
+    }
+    return false;
+}
+
+
