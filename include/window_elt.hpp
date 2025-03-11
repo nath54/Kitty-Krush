b5a511733632bbdbe1768b5c1759cb2d305aa438
nath54 <cerisara.nathan@gmail.com>
2025-03-11 12:24:34 +0100

    Travail

--- include/window_elt.hpp
+++ include/window_elt.hpp
@@ -2,17 +2,46 @@
 #include <vector>
 #include <string>
 #include <map>
+//
+#include "color.hpp"
 
 
 
 //
 class WindowElt{
 
+    public:
+
+        int x;
+        int y;
+        int w = 1;
+        int h = 1;
+
+        //
+        WindowElt(int x, int y, int w = 1, int h = 1)
+            : x(x), y(y), w(w), h(h) {};
+
+        //
+        void draw_elt();
+
+};
+
+
+//
+class WindowEltText : public WindowElt {
 
     public:
 
         //
-        void draw_elt();
+        std::string txt;
+        //
+        Color color;
+        //
+        int fontSize;
+
+        //
+        WindowEltText(std::string txt, Color color, int fontSize, int x, int y, int w = 1, int h = 1)
+        : WindowElt(x, y, w, h), txt(txt), color(color), fontSize(fontSize) {};
 
 };
 
