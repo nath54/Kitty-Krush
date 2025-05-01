3a4f224b9081be5f85cfaa48926de142cb577011
Noza-e <kyraluna.neo@gmail.com>
2025-05-01 15:03:45 +0200

    Clear main

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,18 @@
-//
 #include "main_game.hpp"
-//
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 
 
-int main(int argc, char** argv){
-
-    //
+int main(int argc, char** argv)
+{
     std::srand(std::time({})); // use current time as seed for random generator
-
+    
     // 𝑴𝒆𝒐𝒘. (•- •マ
     std::cout << "Hellow World !\n";
-
-    // Create Main Game
     MainGame main_game = MainGame();
-
-    //
     main_game.mainloop();
 
-    //
     return 0;
 }
