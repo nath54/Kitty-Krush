e12d225bb9871b4d71823d21b58a61b52bd06d9f
nath54 <cerisara.nathan@gmail.com>
2025-05-02 16:10:41 +0200

    Meow

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,19 @@
 int main(int argc, char** argv)
 {
     std::srand(std::time({})); // use current time as seed for random generator
-    
+
     // 𝑴𝒆𝒐𝒘. (•- •マ
-    std::cout << "Hellow World !\n";
+    std::cout << "Hellow World 𓃠 !\n";
+
+    // Initialize the game
     MainGame main_game = MainGame();
+
+    // Mainloop while the game is on
     main_game.mainloop();
 
+    // 𝑴𝒆𝒐𝒘. (•- •マ
+    std::cout << "It was really fun to play with you ! ⸜(｡ ˄ᵕ˄ )⸝♡\n";
+
+    // End of the game
     return 0;
 }
